942a1113a7ec44ae88f2e43076e6f89e87794bb5
lawalOyinlola <oyinlolalawal1705@gmail.com>
2023-09-06 17:32:34 +0000

    ran npm build and deleted app.css file

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -1,6 +1,6 @@
 function StarRating({ rating }) {
-  /*
-    Here's the markup for a single star:
+  
+    // Here's the markup for a single star:
     
     <img
       alt=""
@@ -8,10 +8,10 @@ function StarRating({ rating }) {
       src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
     />
     
-    Your job is to repeat this element
-    based on the `rating` prop.
-    If the rating is 4, we need 4 copies.
-  */
+  //   Your job is to repeat this element
+  //   based on the `rating` prop.
+  //   If the rating is 4, we need 4 copies.
+  // 
 
   return <div className="star-wrapper"></div>;
 }
